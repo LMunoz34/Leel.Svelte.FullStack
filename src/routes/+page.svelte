8115a73397893083ed33fb2f01a02a0920dd1c02
LeelMoon <Leonel.Munoz@apchh.com>
2023-08-14 14:11:09 -0500

    added Google auth instead of anonymous login and now showing posts on the landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,34 @@
 <script>
   import { SignedIn, SignedOut, Doc, Collection } from 'sveltefire';
-  import { signInAnonymously } from "firebase/auth";
+  import { signInAnonymously, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+  var provider = new GoogleAuthProvider();
 </script>
 
-<h3>Leel.Svelte.FullStack</h3>
+<div class="posts-container">
+    <h3><b>Leel.Svelte.FullStack</b></h3>
 
-<SignedIn let:user let:signOut>
-    <p>Hello {user.uid}</p>
-    <button on:click={signOut}>Sign Out</button>
-</SignedIn>
+    <SignedIn let:user let:signOut>
+        <p>Hello {user.uid}</p>
+        <button on:click={signOut}>Sign Out</button>
+    </SignedIn>
 
-<SignedOut let:auth>
-    <button on:click={() => signInAnonymously(auth)}>Sign In</button>
-</SignedOut>
-
-<Doc ref="posts/id" let:data>
+    <SignedOut let:auth>
+        <button on:click={() => signInWithPopup(auth, provider)}>Sign In</button>
+    </SignedOut>
+</div>
+<!-- <Doc ref="posts/D95x9JWsDn0mxpzpvQVS" let:data>
     <h2>{data.title}</h2>
     <p>{data.content}</p>
-</Doc>
+</Doc> -->
 
 <Collection ref="posts" let:data={posts}>
-    {#each posts as post}
-        <h2>{post.title}</h2>
-        <p>{post.content}</p>
-    {/each}
+    <div class="posts-container">
+        {#each posts as post}
+            <div class="card">
+                <h2>{post.title}</h2>
+                <p>{post.content}</p>
+            </div>
+        {/each}
+    </div>
 </Collection>
 
